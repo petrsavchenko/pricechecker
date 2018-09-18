f941e495ce0e1e452dea9b439a1bac0cd4bd76f2
PetrSavchenko <petr.savchenko@plutora.com>
2018-09-19 09:12:03 +1000

    constants of auth0 were changed to prod

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,3 +1,3 @@
-export const API_URL = 'http://localhost:3001';
+export const API_URL = 'https://pricechecker-app.herokuapp.com/';
 export const AUTH_SUCCESSFUL_REDIRECT = '/crawlers';
 export const NOTFOUND = 404;
