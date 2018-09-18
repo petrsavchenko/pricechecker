6452876e9f1472b2c14f38a171d744ed80e14a3f
PetrSavchenko <petr.savchenko@plutora.com>
2018-09-19 09:04:24 +1000

    revert auth0 callbackUrl back to prod

--- client/src/Auth/auth0-variables.js
+++ client/src/Auth/auth0-variables.js
@@ -1,6 +1,6 @@
 export const AUTH_CONFIG = {
   domain: 'petrsavchenko.au.auth0.com',
   clientId: 'Be9fU3WvF5LnAt1YibarSIqQk2XT6GZ3',
-  callbackUrl: 'http://localhost:3000/callback',
+  callbackUrl: 'https://pricechecker-app.herokuapp.com/callback',
   apiUrl: 'https://pricechecker.com'
 }
