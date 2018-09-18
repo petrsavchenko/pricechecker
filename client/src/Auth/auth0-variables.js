08e35a1aae2ced0857df8e645478b6ae19d7938c
PetrSavchenko <petr.savchenko@plutora.com>
2018-09-19 09:24:10 +1000

    change audience back since it is hardcoded on auth0 side

--- client/src/Auth/auth0-variables.js
+++ client/src/Auth/auth0-variables.js
@@ -2,5 +2,5 @@ export const AUTH_CONFIG = {
   domain: 'petrsavchenko.au.auth0.com',
   clientId: 'Be9fU3WvF5LnAt1YibarSIqQk2XT6GZ3',
   callbackUrl: 'https://pricechecker-app.herokuapp.com/callback',
-  apiUrl: 'https://pricechecker-app.herokuapp.com/'
+  apiUrl: 'https://pricechecker.com'
 }
