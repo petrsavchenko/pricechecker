ebff57f5fe9daa90069a458ff0a565cd57956977
PetrSavchenko <petr.savchenko@plutora.com>
2018-09-18 19:38:20 +1000

    last build preparation

--- server.js
+++ server.js
@@ -109,7 +109,7 @@ db.once('open', () => {
   // The "catchall" handler: for any request that doesn't
   // match one above, send back React's index.html file.
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname+'/client/build/index.html'));
+    res.sendFile(path.join(__dirname + '/client/build/index.html'));
   });
 
   //Start crawling
